cc6e7e7950a276b7b95d5cdd0a31de2903bbad97
Renuka-Maddipatla <renuka.maddipatla2002@gmail.com>
2023-10-14 20:58:56 +0530

    pdfQRcode

---
+++ src/main/java/com/pdfQRcode/Generator/PdfQRcodeApplication.java
@@ -0,0 +1,13 @@
+package com.pdfQRcode.Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfQRcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfQRcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdfQRcode/Generator/PdfQRcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfQRcode.Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfQRcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
